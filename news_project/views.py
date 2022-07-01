f01f7735c634dc67456f3451d05354254132f6c5
Dmitri Plotnikov <lightsout1305@gmail.com>
2022-07-02 00:24:51 +0300

    Module D5 has been completed

---
+++ news_project/views.py
@@ -0,0 +1,100 @@
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from .models import Post
+from .filters import PostFilter
+from .forms import PostForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import PermissionRequiredMixin
+
+
+class NewsList(ListView):
+    model = Post
+    ordering = 'register_date'
+    template_name = 'news.html'
+    context_object_name = 'news'
+    paginate_by = 10
+
+
+class NewsSearch(ListView):
+    model = Post
+    ordering = 'register_date'
+    template_name = 'news_search.html'
+    context_object_name = 'news'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class NewsCreate(PermissionRequiredMixin, CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news_edit.html'
+    permission_required = ('news_project.add_post', )
+
+    def form_valid(self, form):
+        news = form.save(commit=False)
+        news.choice = 'NEWS'
+        return super().form_valid(form)
+
+
+class ArticleCreate(PermissionRequiredMixin, CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'article_edit.html'
+    permission_required = ('news_project.add_post', )
+
+    def form_valid(self, form):
+        article = form.save(commit=False)
+        article.choice = 'ARTL'
+        return super().form_valid(form)
+
+
+class NewsUpdate(PermissionRequiredMixin, UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news_edit.html'
+    permission_required = ('news_project.change_post', )
+
+    def form_valid(self, form):
+        news = form.save(commit=False)
+        news.choice = 'NEWS'
+        return super().form_valid(form)
+
+
+class ArticleUpdate(PermissionRequiredMixin, UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'article_edit.html'
+    permission_required = ('news_project.change_post',)
+
+    def form_valid(self, form):
+        article = form.save(commit=False)
+        article.choice = 'ARTL'
+        return super().form_valid(form)
+
+
+class NewsDelete(DeleteView):
+    model = Post
+    template_name = 'news_delete.html'
+    success_url = reverse_lazy('content_list')
+
+
+class ArticleDelete(DeleteView):
+    model = Post
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('content_list')
+
+
+class NewsDetail(DetailView):
+    model = Post
+    template_name = 'detail.html'
+    context_object_name = 'onenews'
+
+
+# Create your views here.
