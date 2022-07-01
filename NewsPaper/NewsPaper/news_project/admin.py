55b9342870ed1f7355fc48cafa8469a8cc90e6fc
lightsout1305 <98709710+lightsout1305@users.noreply.github.com>
2022-07-02 00:59:12 +0300

    Delete NewsPaper/NewsPaper/news_project directory

--- NewsPaper/NewsPaper/news_project/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post, Category, PostCategory
-
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(PostCategory)
-# Register your models here.
