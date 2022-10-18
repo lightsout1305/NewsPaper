2977081e14041705756071cbe1b59ea9a58cb34a
Dmitri Plotnikov <lightsout1305@gmail.com>
2022-10-19 00:30:39 +0300

    Modified detail templates

--- protect/views.py
+++ protect/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.sites.models import Site
 
 
 class IndexView(LoginRequiredMixin, TemplateView):
@@ -8,6 +9,7 @@ class IndexView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['is_not_author'] = not self.request.user.groups.filter(name='authors').exists()
+        context['site'] = Site.objects.get_current().domain
         return context
 
 # Create your views here.
