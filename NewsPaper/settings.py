81f572c31fbc272a7afe4248240963e15a3933ea
Dmitri Plotnikov <lightsout1305@gmail.com>
2022-10-18 23:36:08 +0300

    PostgreSQL database

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -155,10 +155,12 @@ WSGI_APPLICATION = 'NewsPaper.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+
+    "default": env.dj_db_url("DATABASE_URL")
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 CACHES = {
